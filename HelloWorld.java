817daf95e022bb3dab6f06ad382fcb595ddd12ab
Mohammed Tafheemuddin <31982959+tafheem86@users.noreply.github.com>
2022-07-12 20:02:45 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args){
           System.out.println("-----***** Trying Web hooks *****-----");
+		  System.out.println("-----***** Trying Web hooks 2 *****-----");
          		
 	}
 	
